2e739e998a1cc317a50290a703692a0e38d18276
khaledaboshama <khaled.shama-e@maf.ae>
2023-03-30 14:59:10 +0200

    Logic of login user flow

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
-
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
+import 'package:mynotes/views/verify_email.dart';
 
 import 'firebase_options.dart';
 
@@ -15,8 +16,8 @@ void main() async {
     ),
     home: const HomePage(),
     routes: {
-      "/login/" : (context) => const LoginView(),
-      "/register/" : (context) => const RegisterView()
+      "/login/": (context) => const LoginView(),
+      "/register/": (context) => const RegisterView()
     },
   ));
 }
@@ -32,18 +33,16 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-          // final user = FirebaseAuth.instance.currentUser;
-          // print(" user = $user");
-          // if (user?.emailVerified == true) {
-          //   print("email is verified");
-          //   return const Text("done complete");
-          // } else {
-          //   print("email is not verified");
-          //   // Navigator.of(context).push(MaterialPageRoute(
-          //   //     builder: (context) => const VerifyEmailView()));
-          //   return const VerifyEmailView();
-            return const LoginView();
-        // }
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+            return const Text("done");
           default:
             return const CircularProgressIndicator();
         }
