851d1b90c3e4fa942ce07aa78b47ece9eb094bec
khaledaboshama <khaled.shama-e@maf.ae>
2023-04-02 10:57:58 +0200

    Add restart button in verify email screen

--- lib/views/verify_email.dart
+++ lib/views/verify_email.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:mynotes/constants/routes.dart';
 
 class VerifyEmailView extends StatefulWidget {
   const VerifyEmailView({Key? key}) : super(key: key);
@@ -12,7 +13,9 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Verify email"),),
+      appBar: AppBar(
+        title: const Text("Verify email"),
+      ),
       body: Column(
         children: [
           const Text("Please send a verification code"),
@@ -22,7 +25,17 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
                 await user?.sendEmailVerification();
                 print("send a verification email");
               },
-              child: const Text("Send"))
+              child: const Text("Send")
+          ),
+          TextButton(
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut();
+                print("sign out");
+                Navigator.of(context)
+                    .pushNamedAndRemoveUntil(registerRoute, (route) => false);
+              },
+              child: const Text("Restart")
+          )
         ],
       ),
     );
